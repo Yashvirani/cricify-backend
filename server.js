bc94506f278dff0b4ac33fb2e0fc9196de6f8cd3
Yash Virani <viraniyash2001@gmail.com>
2022-12-10 16:27:40 +0530

    commit

--- server.js
+++ server.js
@@ -38,10 +38,10 @@ app.get('/api/config/google', (req, res) => {
 });
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-app.use(express.static(path.join(__dirname, '/frontend/build')));
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
-);
+// app.use(express.static(path.join(__dirname, '/frontend/build')));
+// app.get('*', (req, res) =>
+//   res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+// );
 // app.get('/', (req, res) => {
 //   res.send('Server is ready');
 // });
@@ -124,12 +124,12 @@ io.on('connection', (socket) => {
   });
 });
 
-if (process.env.NODE_ENV == 'production') {
-  app.use(express.static('frontend/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-  });
-}
+// if (process.env.NODE_ENV == 'production') {
+//   app.use(express.static('frontend/build'));
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+//   });
+// }
 
 httpServer.listen(port, () => {
   console.log(`Serve at http://localhost:8080`);
