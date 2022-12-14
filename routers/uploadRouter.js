ab9fba0a0db62da9cc8440c525361ce0d5ea935f
Yash Virani <62023226+Yashvirani@users.noreply.github.com>
2022-12-14 14:09:14 +0530

    Update uploadRouter.js

--- routers/uploadRouter.js
+++ routers/uploadRouter.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import multer from 'multer';
-import multerS3 from 'multer-s3';
+//import multerS3 from 'multer-s3';
 import aws from 'aws-sdk';
 //import config from '../config.js';
 import { v4 as uuidv4 } from 'uuid';
